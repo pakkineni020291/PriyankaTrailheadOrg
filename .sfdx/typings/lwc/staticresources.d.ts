d76671521e5dd0d8482f532ea98ffe90c8d7a5db
Priyanka Akkineni <priyanka.akkineni@ibm.com>
2019-08-21 14:58:31 -0700

    Added mytestclass and other stuff

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/GetAnimalResource" {
+    var GetAnimalResource: string;
+    export default GetAnimalResource;
+}
+declare module "@salesforce/resourceUrl/TestTrailhead" {
+    var TestTrailhead: string;
+    export default TestTrailhead;
+}
